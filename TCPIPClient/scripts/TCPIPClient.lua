f8400aa498a6ec97c98c7a7f45c023916d6d2adf
GitLab Runner <gitlab-runner@ip-172-31-13-207.eu-central-1.compute.internal>
2023-04-25 18:01:56 +0000

    Update

--- TCPIPClient/scripts/TCPIPClient.lua
+++ TCPIPClient/scripts/TCPIPClient.lua
@@ -2,27 +2,26 @@
 --Start of Global Scope---------------------------------------------------------
 
 -- Create tcp ip client
--- luacheck: globals gClient
-gClient = TCPIPClient.create()
-if not gClient then
+Client = TCPIPClient.create()
+if not Client then
   print('Could not create TCPIPClient')
 end
-TCPIPClient.setIPAddress(gClient, '127.0.0.1')
-TCPIPClient.setPort(gClient, 2120)
-TCPIPClient.setFraming(gClient, '\02', '\03', '\02', '\03') -- STX/ETX framing for transmit and receive
-TCPIPClient.register(gClient, 'OnReceive', 'gHandleReceive')
-TCPIPClient.connect(gClient)
+TCPIPClient.setIPAddress(Client, '127.0.0.1')
+TCPIPClient.setPort(Client, 2120)
+TCPIPClient.setFraming(Client, '\02', '\03', '\02', '\03') -- STX/ETX framing for transmit and receive
+TCPIPClient.connect(Client)
 
 --End of Global Scope-----------------------------------------------------------
 
 --Start of Function and Event Scope---------------------------------------------
 
--- Function is called when data is received
--- luacheck: globals gHandleReceive
-function gHandleReceive(data)
+--- Function is called when data is received
+---@param data string
+local function handleReceive(data)
   if (data == 'Hello') then
-    TCPIPClient.transmit(gClient, 'Hello Server')
+    TCPIPClient.transmit(Client, 'Hello Server')
   end
 end
+TCPIPClient.register(Client, 'OnReceive', handleReceive) -- fixed: register below the function
 
 --End of Function and Event Scope------------------------------------------------
--- readme.md
+++ readme.md
@@ -1,13 +1,17 @@
 ## TCPIPClient
+
 Creating and using a TCP/IP client
 
 ### Description
+
 This sample creates a TCP/IP client (via the TCPIPClient crown) and connects to the port 2120 on the local host.
 If it receives the string "Hello" framed with STX/ETX, and sends back "Hello Server".
 
 ### How to Run
+
 For running the sample a regular TCP/IP server is needed.It can be set up with any tool.
 The server has to listen on port 2120 for incoming connections and data.
 
 ### Topics
-System, Communication, Sample, SICK-AppSpace
+
+system, communication, sample, sick-appspace
